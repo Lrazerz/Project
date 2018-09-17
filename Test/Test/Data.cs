73ff5a77352361bbdbdd5fe6eddcac4ca422e94a
Lrazerz <Lrazerz@users.noreply.github.com>
2018-09-17 17:00:23 +0300

    q

---
+++ Test/Test/Data.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace Test
+{
+    class Data
+    {
+        public static string[] Questions = new string[3];
+        public static string[] Answers = new string[3];
+        public static string[] Variant1 = new string[3];
+        public static string[] Variant2 = new string[3];
+        public static string[] Variant3 = new string[3];
+        public static string[] Variant4 = new string[3];
+
+        public static void GetData()
+        {
+            MessageBox.Show("sa");
+            string connectionString = "server=localhost;user=root;database=Testing;password=0000";
+            string Sqlexpression = "SELECT * FROM Questions WHERE Id=2";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                MySqlCommand command = new MySqlCommand(Sqlexpression,connection);
+
+                MySqlDataReader reader = command.ExecuteReader();
+                reader.Close();
+            }
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Test/Test/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        public void Button1(string text)
+        {
+            button1.Text = text;
+        }
+        
+
+    }
+}
