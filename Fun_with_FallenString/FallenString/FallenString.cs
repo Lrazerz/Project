49b6975282e7f8bdcd2c7f7f31bdec4e6ba1a678
Lrazerz <Lrazerz@users.noreply.github.com>
2018-09-17 17:13:43 +0300

    q

---
+++ Fun_with_FallenString/FallenString/FallenString.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace FallenString
+{
+    static class FallenString
+    {
+        static char[] Chinese = new char[] {'Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G',
+                                           'H','J','K','L','Z','X','C','V','B','N','M','1','2','3','4',
+                                           '5','6','7','8','9','0','Й','Ц','У','К','Е','Н','Г','Ш','Щ',
+                                           'З','Х','Ъ','Ф','Ы','В','А','П','Р','О','Л','Д','Ж','Э','Я',
+                                           'Ч','С','М','И','Т','Ь','Б','Ю','№','@','#','%','$','&','?'
+                                            ,'-','=','\''};
+        static object locker = new object();
+        static int ChLength = Chinese.Length - 1;
+        static Random rand = new Random();
+
+        public static void Method(object x)
+        {
+            int maxlength = 29;
+            int stringheight = rand.Next(12, 20);
+            int posx = (int)x;
+            int posy = rand.Next(0, 10);
+            int current = posy;
+
+            for (; ; )
+            {
+                lock (locker)
+                {
+                    if (current == posy)
+                    {
+                        Console.SetCursorPosition(posx, current);
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine(Chinese[rand.Next(0, ChLength)]);
+                        current++;
+                    }
+                    else if (current == posy + 1)
+                    {
+                        Console.SetCursorPosition(posx, current);
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine(Chinese[rand.Next(0, ChLength)]);
+                        Console.SetCursorPosition(posx, current - 1);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(Chinese[rand.Next(0, ChLength)]);
+                        current++;
+                    }
+                    else if (current > posy + 1)
+                    {
+                        if (current < maxlength)
+                        {
+
+                            if (current - posy >= stringheight)
+                            {
+                                Console.SetCursorPosition(posx, current - stringheight);
+                                Console.WriteLine(" ");
+                            }
+                            Console.SetCursorPosition(posx, current);
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine(Chinese[rand.Next(0, ChLength)]);
+                            Console.SetCursorPosition(posx, current - 1);
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine(Chinese[rand.Next(0, ChLength)]);
+                            Console.ForegroundColor = ConsoleColor.DarkGreen;
+                            Console.SetCursorPosition(posx, current - 2);
+                            Console.WriteLine(Chinese[rand.Next(0, ChLength)]);
+                            current++;
+                        }
+                        else
+                        {
+                            for (int i = 0; i < maxlength; i++)
+                            {
+                                Console.SetCursorPosition(posx, i);
+                                Console.WriteLine(" ");
+                                current = posy;
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Fun_with_FallenString/FallenString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace FallenString
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Title = " ";
+            Console.CursorVisible = false;
+            Console.SetBufferSize(100, 30);
+            Console.SetWindowSize(100, 30);
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Press any key");
+            Console.ReadKey();
+            Console.Clear();
+            for (int i = 0; i < 100; i += 4)
+            {
+                new Thread(new ParameterizedThreadStart(FallenString.Method)).Start(i);
+                Thread.Sleep(10);
+            }
+            for (int i = 2; i < 100; i += 4)
+            {
+                new Thread(new ParameterizedThreadStart(FallenString.Method)).Start(i);
+                Thread.Sleep(40);
+            }
+        }
+    }
+}
